71873d9a7a054837b7a7302323330e0348b8a565
Lac <laclys@126.com>
2021-05-26 22:56:14 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import './app.scss'
+import { useState } from 'react'
+
+export default () => {
+  return <div className="app-home">hello</div>
+}
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+import 'antd/dist/antd.min.css'
+
+ReactDOM.render(<App />, document.getElementById('app'))
