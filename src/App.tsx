f40ff3c97a718ca0304ab18fb0dbbceefb0b77c1
Lac <laclys@126.com>
2021-05-30 00:29:19 +0800

    feat: use usecallbackRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,73 @@
 import { useState, useRef } from 'react'
+import { useCallbackRef } from './packages/hooks/useCallbackRef'
 
 import './app.scss'
 
 export default () => {
-
   const [pos, setPos] = useState({
     top: 0,
-    left: 0
+    left: 0,
   })
 
-  const moveDraggier = (() => {
+  const posRef = useRef(pos)
+  posRef.current = pos
 
+  const moveDraggier = (() => {
     const currentPos = useRef({
       sTop: 0,
       sLeft: 0,
       sX: 0,
-      sY: 0
+      sY: 0,
     })
 
-    const mouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    const mouseDown = useCallbackRef((e: React.MouseEvent<HTMLDivElement>) => {
       document.addEventListener('mousemove', mouseMove)
       document.addEventListener('mouseup', mouseUp)
       currentPos.current = {
         sTop: pos.top,
         sLeft: pos.left,
         sX: e.clientX,
-        sY: e.clientY
+        sY: e.clientY,
       }
-    }
+    })
 
-    const mouseMove = (e: MouseEvent) => {
+    const mouseMove = useCallbackRef((e: MouseEvent) => {
+      console.log('pos', pos, posRef.current)
+
       const { sTop, sLeft, sX, sY } = currentPos.current
       const durX = e.clientX - sX
       const durY = e.clientY - sY
       setPos({
         top: sTop + durY,
-        left: sLeft + durX
+        left: sLeft + durX,
       })
-    }
+    })
 
-    const mouseUp = (e: MouseEvent) => {
+    const mouseUp = useCallbackRef((e: MouseEvent) => {
       document.removeEventListener('mousemove', mouseMove)
       document.removeEventListener('mouseup', mouseUp)
-    }
+    })
 
     return {
-      mouseDown
+      mouseDown,
     }
-
   })()
 
-
-  return <div className="app-home">
-    <div >hello world</div>
-    <div style={{
-      display: 'inline-flex',
-      position: 'relative',
-      top: `${pos.top}px`,
-      left: `${pos.left}px`,
-      width: '100px',
-      height: '100px',
-      background: 'blue',
-    }}
-    onMouseDown={moveDraggier.mouseDown}
-    
-    ></div>
-  </div>
+  return (
+    <div className="app-home">
+      <div>hello world</div>
+      <div
+        style={{
+          display: 'inline-flex',
+          position: 'relative',
+          top: `${pos.top}px`,
+          left: `${pos.left}px`,
+          width: '100px',
+          height: '100px',
+          background: 'blue',
+        }}
+        onMouseDown={moveDraggier.mouseDown}
+      ></div>
+    </div>
+  )
 }
